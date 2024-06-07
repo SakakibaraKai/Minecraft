5d939259a3272476aa0509372261fdb63c441630
Kai Depweg <115040382+SakakibaraKai@users.noreply.github.com>
2024-06-07 03:29:51 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
 
+resource "aws_instance" "minecraft" {
+  ami           = "ami-01cd4de4363ab6ee8"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "MinecraftServer"
+  }
+}
