bfc536ffa17c96be81f78f04ee9a87af9bafb5f4
Kai Depweg <115040382+SakakibaraKai@users.noreply.github.com>
2024-06-07 21:29:32 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "minecraft" {
-  ami           = "ami-01cd4de4363ab6ee8"
-  instance_type = "t3.small"
+  ami           = "ami-05a6dba9ac2da60cb"
+  instance_type = "t4g.small"
+  security_groups = [aws_security_group.minecraft.name]
+  associate_public_ip_address = true
 
   tags = {
     Name = "MinecraftServer"
+  }
+}
+
+resource "aws_security_group" "minecraft" {
+  name        = "Minecraft_Security_Group"
+  description = "Security group for minecraft server"
+  vpc_id      = "vpc-0d7050b9b79c37ac1"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
