ee1a4a493c08e353fcfb7697388a49620b69be75
Kai Depweg <115040382+SakakibaraKai@users.noreply.github.com>
2024-06-09 17:11:25 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,56 +2,9 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "minecraft_server" {
-  ami           = "ami-05a6dba9ac2da60cb"
-  instance_type = "t4g.small"
-  key_name      = var.key_name
-  security_groups = [aws_security_group.minecraft.name]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Minecraft Server West"
-  }
-
-  provisioner "file" {
-    source      = "../scripts/setup-minecraft.sh"
-    destination = "/home/ec2-user/setup-minecraft.sh"
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file(var.private_key_path)
-      host        = self.public_ip
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install -y dos2unix",
-      "until command -v dos2unix >/dev/null 2>&1; do sleep 1; done",
-      "dos2unix /home/ec2-user/setup-minecraft.sh",
-      "chmod +x /home/ec2-user/setup-minecraft.sh",
-      "sudo /home/ec2-user/setup-minecraft.sh"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file(var.private_key_path)
-      host        = self.public_ip
-    }
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file(var.private_key_path)
-    host        = self.public_ip
-  }
-}
-
-resource "aws_security_group" "minecraft" {
+resource "aws_security_group" "Minecraft_Security_Group1" {
   name        = "Minecraft_Security_Group1"
   description = "Security group for minecraft server"
-  vpc_id      = "vpc-0d7050b9b79c37ac1"
 
   ingress {
     from_port   = 25565
@@ -72,5 +25,16 @@ resource "aws_security_group" "minecraft" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "minecraft_server" {
+  ami           = "ami-05a6dba9ac2da60cb"
+  instance_type = "t4g.small"
+  key_name      = "lab6"
+  vpc_security_group_ids = [aws_security_group.Minecraft_Security_Group1.id]
+
+  tags = {
+    Name = "Minecraft Server West"
   }
 }
